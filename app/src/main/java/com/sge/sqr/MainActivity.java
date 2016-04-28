56cbebfe967b9669da1fc9e7fcad147583124494
hbrunet <hectorbrunet@gmail.com>
2016-04-28 12:06:36 -0300

    .

--- app/src/main/java/com/sge/sqr/MainActivity.java
+++ app/src/main/java/com/sge/sqr/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.SparseArray;
 import android.view.Menu;
@@ -64,8 +65,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                resultWS.setText("");
-                barcodeInfo.setText(R.string.escanea_el_codigo);
+                try {
+                    resultWS.setText("");
+                    barcodeInfo.setText(R.string.escanea_el_codigo);
+                } catch (Exception ex) {
+                    Snackbar.make(v, ex.getMessage(), Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
             }
         });
 
